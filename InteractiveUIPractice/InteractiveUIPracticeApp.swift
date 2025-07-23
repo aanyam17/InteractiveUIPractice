5c1595c2273b4d9806ef819ceecfd96b460e0f8b
Aanya <aanya.malyala@gmail.com>
2025-07-23 16:38:23 -0400

    Initial Commit

---
+++ InteractiveUIPractice/InteractiveUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveUIPracticeApp.swift
+//  InteractiveUIPractice
+//
+//  Created by Aanya Malyala on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
